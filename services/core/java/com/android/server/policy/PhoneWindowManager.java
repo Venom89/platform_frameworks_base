087b622249054d6141ca6b7bf333410898dba86a
Kyrylo Mikos <kiril.mik.os@gmail.com>
2014-12-07 13:51:00 +0200

    frameworks/base: Don't report existing menu key if it was remapped. [2/3]
    Change-Id: Iabad06cd42cebecdff6f0440899432513681b063
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -799,6 +799,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     private HardkeyActionHandler mKeyHandler;
 
+    private boolean mHasPermanentMenuKey;
+
     private class PolicyHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
@@ -866,22 +868,21 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 case MSG_DISPOSE_INPUT_CONSUMER:
                     disposeInputConsumer((InputConsumer) msg.obj);
                     break;
-                }
                 case HardkeyActionHandler.MSG_FIRE_HOME:
                     launchHomeFromHotKey();
                     break;
-//                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
-//                    synchronized (mLock) {
-//                        mHasPermanentMenuKey = msg.arg1 == 1;
-//                    }
-//                    break;
+                case HardkeyActionHandler.MSG_UPDATE_MENU_KEY:
+                    synchronized (mLock) {
+                        mHasPermanentMenuKey = msg.arg1 == 1;
+                    }
+                    break;
                 case HardkeyActionHandler.MSG_DO_HAPTIC_FB:
                     performHapticFeedbackLw(null,
                             HapticFeedbackConstants.LONG_PRESS, false);
                     break;
+		}
             }
         }
-   }
 
     private DUSystemReceiver mDUReceiver = new DUSystemReceiver() {
         @Override
@@ -8235,9 +8236,16 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         return mNavbarVisible;
     }
 
+    // reflects factory or emulator prop override
+    // basically a cm compatability shim
     @Override
     public boolean needsNavigationBar() {
         return mHasNavigationBar;
+    }
+
+    @Override
+    public boolean hasPermanentMenuKey() {
+        return !hasNavigationBar() && mHasPermanentMenuKey;
     }
 
     @Override
