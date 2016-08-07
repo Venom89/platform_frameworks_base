1b2bb3df89b5e5c5b22d4bf839f8cab3ce8cc962
Randall Rushing <randall.rushing@gmail.com>
2016-08-07 12:06:07 -0700

    Buttons: Hardware key disable support with M adaptations [1/2]
    Change-Id: Ib876f6ef50e9ba42e9aa862f50e871b3f1e39ae0
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -5958,6 +5958,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    private boolean isHwKeysDisabled() {
+        return mKeyHandler != null ? mKeyHandler.isHwKeysDisabled() : false;
+    }
+
     /** {@inheritDoc} */
     @Override
     public int interceptKeyBeforeQueueing(KeyEvent event, int policyFlags) {
@@ -6041,7 +6045,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
         boolean useHapticFeedback = down
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
-                && event.getRepeatCount() == 0;
+                && event.getRepeatCount() == 0
+                && !isHwKeysDisabled();
 
         // Specific device key handling
         if (mDeviceKeyHandler != null) {
